97fc11c62ed6f98eef19fc8e001fe3d8a5c49594
FR <naoki.maekawa.511@gmail.com>
2023-11-17 20:34:26 +0900

    11/17

---
+++ app.js
@@ -0,0 +1,108 @@
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const ejsMate = require(`ejs-mate`);
+const { campgroundSchema } = require('./Schemas')
+const catchAsync = require(`./utils/catchAsync`);
+const ExpressError = require(`./utils/ExpressError`)
+const methodOverride = require('method-override');
+const Campground = require('./models/campground');
+const { get } = require('https');
+const { error } = require('console');
+
+
+mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp',
+    { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+    .then(() => {
+        console.log(`MongoDBコネクションOK！`);
+    })
+    .catch(err => {
+        console.log(`MongoDBコネクションエラー！`);
+        console.log(err);
+    });
+
+
+const app = express();
+
+app.engine(`ejs`, ejsMate);
+app.set(`view engine`, `ejs`);
+app.set(`views`, path.join(__dirname, `views`));
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride(`_method`));
+
+const validateCampground = (req, res, next) => {
+    const {error} = campgroundSchema.validate(req.body);
+    if(error) {
+        const msg = error.details.map(detail => detail.message).join(`,`)
+        throw new ExpressError(msg, 400);
+    } else {
+        next();
+    }
+}
+
+app.get('/', (req, res) => {
+    res.render(`home`)
+});
+
+app.get('/campgrounds', async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render(`campgrounds/index`, { campgrounds });
+});
+
+app.get('/campgrounds/new', (req, res) => {
+    res.render(`campgrounds/new`);
+});
+
+app.get('/campgrounds/:id', catchAsync(async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render(`campgrounds/show`, { campground });
+}));
+
+app.post('/campgrounds', validateCampground ,catchAsync(async (req, res, next) => {
+    // if (!req.body.campground) throw new ExpressError(`不正なキャンプ場のデータです`, 400);
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+}));
+
+app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render(`campgrounds/edit`, { campground });
+}));
+
+app.put('/campgrounds/:id', validateCampground ,catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+    res.redirect(`/campgrounds/${campground._id}`);
+}));
+
+app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
+    const { id } = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect(`/campgrounds`);
+}));
+
+app.all(`*`, (req, res, next) => {
+    next(new ExpressError(`ページが見つかりませんでした`, 404))
+})
+
+app.use((err, req, res, next) => {
+    const { statusCode = 500, message = `問題が起きました` } = err;
+    res.status(statusCode).render(`error`, { err });
+});
+// ↓
+
+// app.use((err, req, res, next) => {
+//     const { statusCode = 500} = err;
+//     if(!err.message) {
+//         err.message = `問題が起きました`;
+//     }
+//     res.status(statusCode).render(`error`, { err });
+// });
+
+
+app.listen(3000, () => {
+    console.log(`ポート3000でリクエスト待受中`);
+});
---
+++ seeds/index.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const cities = require('./cities');
+const { descriptors, places } = require('./seedHelpers');
+const Campground = require('../models/campground');
+
+
+mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp', 
+{ useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+    .then(() => {
+        console.log(`MongoDBコネクションOK！`);
+    })
+    .catch(err => {
+        console.log(`MongoDBコネクションエラー！`);
+        console.log(err);
+    });
+
+
+const sample = array => array[Math.floor(Math.random() * array.length)]
+
+
+const seedDB = async () => {
+   await Campground.deleteMany({});
+   for (let i = 0; i < 50; i++) {
+    const randomCityIndex = Math.floor(Math.random() * cities.length);
+    const price = Math.floor(Math.random() * 2000) + 1000;
+    const camp = new Campground({
+        location: `${cities[randomCityIndex].prefecture}${cities[randomCityIndex].city}`,
+        title: `${sample(descriptors)}・${sample(places)}`,
+        image: `https://source.unsplash.com/collection/483251`,
+        description:`木曾路はすべて山の中である。あるところは岨づたいに行く崖の道であり、あるところは数十間の深さに臨む木曾川の岸であり、あるところは山の尾をめぐる谷の入り口である。`,
+        price
+        // price:price　　と書くこともできるが省略表記を使っている
+    });
+    await camp.save();
+   }
+}
+
+seedDB().then(() => {
+    mongoose.connection.close();
+});
