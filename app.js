ea4eb7640d955026b040b50864b29182cbcc92d6
FR <naoki.maekawa.511@gmail.com>
2023-11-24 09:55:03 +0900

    11/24

--- app.js
+++ app.js
@@ -66,7 +66,8 @@ app.get('/campgrounds/new', (req, res) => {
 });
 
 app.get('/campgrounds/:id', catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id);
+    const campground = await Campground.findById(req.params.id).populate(`reviews`);
+    console.log(campground);
     res.render(`campgrounds/show`, { campground });
 }));
 
