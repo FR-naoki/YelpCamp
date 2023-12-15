ea627db086a3e3bdcebe9e8114479ad71b0f6e6b
FR <naoki.maekawa.511@gmail.com>
2023-12-15 23:21:20 +0900

    12/15 517

--- middleware.js
+++ middleware.js
@@ -1,3 +1,8 @@
+const ExpressError = require(`./utils/ExpressError`);
+const Campground = require('./models/campground');
+const { campgroundSchema } = require('./Schemas');
+
+
 module.exports.isLoggedIn = (req, res, next) => {
     console.log(`req.user`, req.user);
     if (!req.isAuthenticated()) {
@@ -7,3 +12,23 @@ module.exports.isLoggedIn = (req, res, next) => {
     }
     next();
 }
+
+module.exports.validateCampground = (req, res, next) => {
+    const { error } = campgroundSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(detail => detail.message).join(`,`)
+        throw new ExpressError(msg, 400);
+    } else {
+        next();
+    }
+}
+
+module.exports.isAuthor = async (req, res, next) => {
+    const {id} = req.params;
+    const campground = await Campground.findById(id);
+    if (!campground.author.equals(req.user._id)) {
+        req.flash(`error`, `そのアクションの権限がありません`)
+        return res.redirect(`/campgrounds/${id}`);
+    }
+    next();
+};
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -4,18 +4,7 @@ const catchAsync = require(`../utils/catchAsync`);
 const ExpressError = require(`../utils/ExpressError`);
 const Campground = require('../models/campground');
 const { campgroundSchema } = require('../Schemas');
-const { isLoggedIn } = require(`../middleware`);
-
-
-const validateCampground = (req, res, next) => {
-    const { error } = campgroundSchema.validate(req.body);
-    if (error) {
-        const msg = error.details.map(detail => detail.message).join(`,`)
-        throw new ExpressError(msg, 400);
-    } else {
-        next();
-    }
-}
+const { isLoggedIn, isAuthor, validateCampground } = require(`../middleware`);
 
 router.get('/', async (req, res) => {
     const campgrounds = await Campground.find({});
@@ -45,33 +34,24 @@ router.post('/', isLoggedIn, validateCampground, catchAsync(async (req, res, nex
     res.redirect(`/campgrounds/${campground._id}`);
 }));
 
-router.get('/:id/edit' , isLoggedIn, catchAsync(async (req, res) => {
+router.get('/:id/edit' , isLoggedIn, isAuthor, catchAsync(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findById(id);
     if (!campground) {
         req.flash(`error`, `キャンプ場は見つかりませんでした`);
         return res.redirect(`/campgrounds`);
     }
-    if (!campground.author.equals(req.user._id)) {
-        req.flash(`error`, `更新する権限がありません`)
-        return res.redirect(`/campgrounds/${id}`);
-    }
     res.render(`campgrounds/edit`, { campground });
 }));
 
-router.put('/:id', isLoggedIn, validateCampground, catchAsync(async (req, res) => {
+router.put('/:id', isLoggedIn, isAuthor, validateCampground, catchAsync(async (req, res) => {
     const { id } = req.params;
-    const campground = await Campground.findById(id);
-    if (!campground.author.equals(req.user._id)) {
-        req.flash(`error`, `更新する権限がありません`)
-        return res.redirect(`/campgrounds/${id}`);
-    }
-    const camp = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
     req.flash(`success`, `キャンプ場を更新しました`);
     res.redirect(`/campgrounds/${campground._id}`);
 }));
 
-router.delete('/:id', isLoggedIn, catchAsync(async (req, res) => {
+router.delete('/:id', isLoggedIn, isAuthor, catchAsync(async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
     req.flash(`success`, `キャンプ場を削除しました`);
