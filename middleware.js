84b1a18e0568949c548e623387497c01fb76e18d
FR <naoki.maekawa.511@gmail.com>
2023-12-18 19:33:29 +0900

    12/18 519

--- middleware.js
+++ middleware.js
@@ -1,6 +1,7 @@
 const ExpressError = require(`./utils/ExpressError`);
 const Campground = require('./models/campground');
 const { campgroundSchema, reviewSchema } = require('./Schemas');
+const Review = require('./models/review');
 
 
 module.exports.isLoggedIn = (req, res, next) => {
@@ -27,6 +28,16 @@ module.exports.isAuthor = async (req, res, next) => {
     const {id} = req.params;
     const campground = await Campground.findById(id);
     if (!campground.author.equals(req.user._id)) {
+        req.flash(`error`, `そのアクションの権限がありません`)
+        return res.redirect(`/campgrounds/${id}`);
+    }
+    next();
+};
+
+module.exports.isReviewAuthor = async (req, res, next) => {
+    const { id, reviewId } = req.params;
+    const review = await Review.findById(reviewId);
+    if (!review.author.equals(req.user._id)) {
         req.flash(`error`, `そのアクションの権限がありません`)
         return res.redirect(`/campgrounds/${id}`);
     }
