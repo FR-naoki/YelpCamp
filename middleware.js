a3ae8beb438fadbe98b0696af95320115e7fc3c2
FR_naoki <naoki.maekawa.511@gmail.com>
2024-03-17 10:53:08 +0900

    3/17

--- middleware.js
+++ middleware.js
@@ -20,15 +20,3 @@ module.exports.validateCampground = (req, res, next) => {
         const msg = error.details.map(detail => detail.message).join(`,`)
         throw new ExpressError(msg, 400);
     } else {
-        next();
-    }
-}
-
-
-
-module.exports.isReviewAuthor = async (req, res, next) => {
-    const { id, reviewId } = req.params;
-    const review = await Review.findById(reviewId);
-    if (!review.author.equals(req.user._id)) {
-        req.flash(`error`, `そのアクションの権限がありません`)
-        return res.redirect(`/campgrounds/${id}`);
